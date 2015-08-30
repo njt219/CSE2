3401c0de825a27dd2de5eef8862502b10a3b3d87
njt219 <njt219@lehigh.edu>
2015-08-30 20:33:31 +0000

    updated WelcomeClass

--- hw01/WelcomeClass.java
+++ hw01/WelcomeClass.java
@@ -14,9 +14,9 @@ public class WelcomeClass{
    System.out.println("             |      Welcome      |          ");
    System.out.println("              - - - - - - - - - -           ");
    System.out.println("              ^  ^  ^  ^  ^  ^              ");
-   System.out.println("             / \/ \/ \/ \/ \/ \             ");
+   System.out.println("              \\/ \\/ \\/ \\/ \\/ \\/ \\ ");
    System.out.println("             |-N--J--T--2--1--9-|           ");
-   System.out.println("             \ /\ /\ /\ /\ /\ /             ");
+   System.out.println("             \\ /\\ /\\ /\\ /\\ /\\ /       ");
    System.out.println("              v  v  v  v  v  v              ");
    System.out.println("     I have dual citizenship in US and germany.  ");
    
