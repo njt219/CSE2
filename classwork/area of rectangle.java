3885f9aa429a33652405ce8696bddb6d7d807e94
njt219 <njt219@lehigh.edu>
2015-09-03 15:39:26 +0000

    updated Cyclometer

---
+++ classwork/area of rectangle.java
@@ -0,0 +1,10 @@
+public class area{
+public static void main(String[] args) {
+int width = 10;
+int height = 15;
+int area = width * height;
+
+System.out.println("Area is " + area);
+
+}
+}
---
+++ classwork/bankaccount.java
@@ -0,0 +1,9 @@
+public class bankaccount{
+public static void main(String[] args) {
+int balance = 1000; 
+double interest_rate = .1; 
+double balance_with_interest = balance * interest_rate + balance ; 
+
+System.out.println("The balance with interest is " + balance_with_interest );
+}
+}
